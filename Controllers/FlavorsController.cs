07c380801bb391ac1d8c33b39687448960bc1e3e
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-06-03 10:45:40 -0700

    Route the Details View to the Flavor model

--- Controllers/FlavorsController.cs
+++ Controllers/FlavorsController.cs
@@ -30,6 +30,14 @@ namespace  Pierre.Controllers
         return RedirectToAction("Index");
     
     }
+    public ActionResult Details( int id)
+    {
+      var thisFlavor =_db.Flavors
+          .Include(flavor => flavor.JoinEntities)
+          .ThenInclude(join => join.Treat)
+          .FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
   }
   
 }
