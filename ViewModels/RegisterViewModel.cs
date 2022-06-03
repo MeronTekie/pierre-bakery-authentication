b96bbf05b9f202ea47fb6b0955f3faa74ce68c41
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-06-03 10:19:05 -0700

    Create the View files for the falvor class

--- ViewModels/RegisterViewModel.cs
+++ ViewModels/RegisterViewModel.cs
@@ -15,8 +15,8 @@ namespace Pierre.ViewModels
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.Please Enter a valid Password.")]
         public string ConfirmPassword { get; set; }
     }
 }
